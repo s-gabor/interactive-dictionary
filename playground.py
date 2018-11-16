7f5eef28500ef7fd8ede2277cd5584c6898c718e
s-gabor <ing.gabor@yahoo.com>
2018-11-16 10:34:52 +0200

    added word suggestion

--- playground.py
+++ playground.py
@@ -1,6 +1,7 @@
 # playground.py
 
 import constants
+from difflib import get_close_matches
 
 print('This is an english dictionary.')
 
@@ -12,5 +13,13 @@ while True:
     if word in dictionary:
         print(dictionary[word])
     else:
-        print('That\'s not an english word. Try again!')
+        closest_word = get_close_matches(word, dictionary, n=1, cutoff=0.6)
+        if closest_word:
+            confirmation = input(f'I\'m not familiar with that word. Did you mean "{closest_word[0]}" instead(y/n)? ')
+            if confirmation[0] in 'Yy':
+                print(dictionary[closest_word[0]])
+            else:
+                print('Sorry I couldn\'t be more helpful.')
+        else:
+            print('That\'s not an english word! Try again.')
 print('Thanks! Have a nice day.\n')
