b81de4a71944c2c9169462810fb618e8490015fe
s-gabor <ing.gabor@yahoo.com>
2018-11-18 10:06:01 +0200

    moved the code to main

--- constants.py
+++ constants.py
@@ -1,3 +1,6 @@
 # constants.py
 
+import json
+
 fname = 'data_files/data.json'
+data = json.load(open(fname, 'r'))
--- playground.py
+++ playground.py
@@ -4,14 +4,8 @@ import constants
 import json
 from difflib import get_close_matches
 
-print('This is an english dictionary.')
 
-dictionary = json.load(open(constants.fname, 'r'))
-while True:
-    word = input('Enter a word("q" to exit the program): ')
-    if word == 'q':
-        break
-    output = None
+def is_in_dictionary(word):
     if word in dictionary:
         pass
     elif word.capitalize() in dictionary:
@@ -21,11 +15,26 @@ while True:
     elif word.upper() in dictionary:
         word = word.upper()
     else:
-        closest_word = get_close_matches(word, dictionary, n=1, cutoff=0.6)
+        word = None
+    return word
+
+
+dictionary = json.load(open(constants.fname, 'r'))
+print('This is an english dictionary.')
+
+while True:
+    user_input = input('Enter a word("q" to exit the program): ')
+    if user_input == 'q':
+        break
+    output = None
+    if is_in_dictionary(user_input):
+        output = dictionary[user_input]
+    else:
+        closest_word = get_close_matches(user_input, dictionary, n=1, cutoff=0.6)
         if closest_word:
             confirmation = input(f'I\'m not familiar with that word. Did you mean "{closest_word[0]}" instead(y/n)? ')
             if confirmation[0] in 'Yy':
-                word = closest_word[0]
+                user_input = closest_word[0]
             else:
                 output = 'Sorry I couldn\'t be more helpful.'
         else:
@@ -34,13 +43,13 @@ while True:
     if output:
         print(output)
     else:
-        output = dictionary[word]
+        output = dictionary[user_input]
         if len(output) == 1:
-            print(f'"{word}":')
+            print(f'"{user_input}":')
             print(f'   {output[0]}')
             print()
         else:
-            print(f'"{word}" has multiple meanings:')
+            print(f'"{user_input}" has multiple meanings:')
             for i in range(1, len(output)+1):
                 print(f'   {i}.', output[i-1])
             print()
